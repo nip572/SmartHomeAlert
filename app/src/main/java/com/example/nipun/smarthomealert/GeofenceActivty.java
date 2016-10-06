88551a4b66f9142eac996f429d4dfc3a192833e1
dhwanishah9 <dhwani.shah0912@gmail.com>
2016-10-05 21:58:38 -0700

    Adding files for geofence implementation

---
+++ app/src/main/java/com/example/nipun/smarthomealert/GeofenceActivty.java
@@ -0,0 +1,13 @@
+package com.example.nipun.smarthomealert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeofenceActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geofence_activty);
+    }
+}
