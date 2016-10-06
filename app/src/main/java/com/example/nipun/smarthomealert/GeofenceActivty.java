008b4b9fc7b907c6166a9df25d48428df579ac65
dhwanishah9 <dhwani.shah0912@gmail.com>
2016-10-05 22:20:08 -0700

    Adding services and permission for geofencing

--- app/src/main/java/com/example/nipun/smarthomealert/GeofenceActivty.java
+++ app/src/main/java/com/example/nipun/smarthomealert/GeofenceActivty.java
@@ -3,6 +3,9 @@ package com.example.nipun.smarthomealert;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created by Dhwani on 05/10/16.
+ */
 public class GeofenceActivty extends AppCompatActivity {
 
     @Override
