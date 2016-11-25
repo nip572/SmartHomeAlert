d3d887e11ae2bbd75d247d89186d63c2b7677b58
nip572 <nipun.ahuja572@gmail.com>
2016-11-24 18:14:57 -0800

    Location Service monitors location after every 3 seconds

--- app/src/main/java/com/example/nipun/smarthomealert/BaseActivity.java
+++ app/src/main/java/com/example/nipun/smarthomealert/BaseActivity.java
@@ -1,11 +1,17 @@
 package com.example.nipun.smarthomealert;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -19,9 +25,12 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import static com.example.nipun.smarthomealert.R.id.textView;
 
 public class BaseActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,6 +42,8 @@ public class BaseActivity extends AppCompatActivity
     TextView displayName;
     TextView displayEmailId;
     NavigationView navigationView;
+    private BroadcastReceiver broadcastReceiver;
+
 
 
 
@@ -43,6 +54,15 @@ public class BaseActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
        // Button button = (Button) findViewById(R.id.button2);
         setSupportActionBar(toolbar);
+
+        //Start Location Service
+        Intent i =new Intent(getApplicationContext(),ServiceLocation.class);
+        startService(i);
+
+        if(!runtime_permissions()){
+
+
+    }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -152,6 +172,8 @@ public class BaseActivity extends AppCompatActivity
 
 
 
+
+
     private void signOut() {
         Log.d("base act sign out check" , "SIGNED OUT 1");
         FirebaseAuth.getInstance().signOut();
@@ -162,8 +184,43 @@ public class BaseActivity extends AppCompatActivity
         BaseActivity.this.startActivity(startLogin);
         Toast tost2 = Toast.makeText(context,"Signed Out Successfully" , Toast.LENGTH_SHORT );
         tost2.show();
+    }
+    private boolean runtime_permissions() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+
+            return true;
+        }
+        return false;
+    }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if( grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+
+            }else {
+                runtime_permissions();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Log.d(intent.getExtras().get("coordinates").toString(), "onReceive: ");
+
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
     }
 
 }
